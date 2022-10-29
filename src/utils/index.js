f1cc93316c7157c60355ff003590a4949104beff
anasofiabeltran <anas.beltran206@gmail.com>
2022-10-29 14:07:06 -0500

    arreglo deploy

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,7 @@
 //url base
 //let BASE = "https://infinite-cove-11379.herokuapp.com/api/"
 //let BASE = "http://localhost:3001/api";
-let BASE = "https://creativebackend.herokuapp.com/";
+let BASE = "https://creativebackend.herokuapp.com/api";
 
 
 function createUser(user,url){
